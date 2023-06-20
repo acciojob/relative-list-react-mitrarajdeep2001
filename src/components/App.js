101e1924a61d0a141f2e5246edcf5d62a24c94c8
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-06-20 11:44:46 +0000

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+              <ol id='relativeList'>
+                <li id='relativeListItem1'>Raj</li>
+                <li id='relativeListItem2'>Priya</li>
+                <li id='relativeListItem3'>Anuj</li>
+                <li id='relativeListItem4'>Rohan</li>
+              </ol>
     </div>
   )
 }
